4b1410517dd8a428dbc54b18b822107b64a20e6a
Logan Schwartz <loganschwartz@hotmail.com>
2018-07-22 00:18:09 -0400

    Fixing pyton typo error

--- tv_grab_nl.py
+++ tv_grab_nl.py
@@ -1185,10 +1185,10 @@ def main():
 
     # check Python version
     if sys.version_info[:2] < (2,6):
-        log("tv_grab_nl_py requires Pyton 2.6 or higher\n")
+        log("tv_grab_nl_py requires Python 2.6 or higher\n")
         return(2)
     elif sys.version_info[:2] >= (3,0):
-        log("tv_grab_nl_py does not yet support Pyton 3 or higher.\nExpect errors while we proceed\n")
+        log("tv_grab_nl_py does not yet support Python 3 or higher.\nExpect errors while we proceed\n")
 
     # hpath = ''
     xmltv_dir   = hpath+'/.xmltv'
